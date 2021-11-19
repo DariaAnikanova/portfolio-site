6fe8521a6abfc8b60572ebe2dcb040aa454a304f
Daria Anikanova <daria.i.anikanova@gmail.com>
2021-11-19 16:57:22 -0500

    update

--- js/main.js
+++ js/main.js
@@ -1,1 +1,5 @@
+//If using links with role=button, have to add key event handler that listens for the space key in order to be consistent with native buttons.
 
+const navList = document.querySelector(".header_nav_list");
+const navButton = document.querySelectorAll(".nav-button");
+const pageContent = document.querySelectorAll(".");
