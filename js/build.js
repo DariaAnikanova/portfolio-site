8fd0877be78be48f0d845fa184e8e8759908b7c8
Daria Anikanova <daria.i.anikanova@gmail.com>
2021-12-09 17:57:39 -0500

    Update

--- js/build.js
+++ js/build.js
@@ -1,6 +1,13 @@
 const Mustache = require("mustache");
 
 const siteContent = require("../content.json");
-const view = {};
+const content = JSON.parse(siteContent);
+
+const view = {
+  content,
+  title: function () {
+    return content.firstName + " " + content.lastName + "Portfolio";
+  },
+};
 
 const output = Mustache.render("", view);
