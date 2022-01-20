a91b4f78493cc5c4ce6f8f3b79f271c24ae9b7be
DariaAnikanova <daria.i.anikanova@gmail.com>
2022-01-20 18:43:00 -0500

    mobile nav WIP

--- js/main.js
+++ js/main.js
@@ -1,8 +1,17 @@
 //If using links with role=button, have to add key event handler that listens for the space key in order to be consistent with native buttons.
 
 // Page Tabs //
-const navList = document.querySelector(".header_nav_list");
-const navButton = document.querySelectorAll(".nav-button");
-// const pageContent = document.querySelectorAll(".");
+// const mobile = document.getElementById('mobile');
 
+// var burgerBtn = document.getElementById('burgerBtn');
+// var mobile = document.getElementById('mobile');
+// var demo1 = document.getElementById('demo1');
+// var demo2 = document.getElementById('demo2');
+// var demo3 = document.getElementById('demo3');
+
+// burgerBtn.addEventListener('click', function() {
+//   mobile.classList.toggle('navigation');
+// }, false);
+
+// mobile.classList.toggle('navigation');
 // Project List //
