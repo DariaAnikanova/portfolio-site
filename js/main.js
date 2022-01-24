b106642e6f227ea172605be218b68e360ad248d0
DariaAnikanova <daria.i.anikanova@gmail.com>
2022-01-24 18:27:53 -0500

    WIP

--- js/main.js
+++ js/main.js
@@ -1,17 +1,23 @@
 //If using links with role=button, have to add key event handler that listens for the space key in order to be consistent with native buttons.
+function toggleNavMenuIcon() {
+  burgerBtn.classList.toggle("fas");
+  burgerBtn.classList.toggle("fa-bars");
+  burgerBtn.classList.toggle("far");
+  burgerBtn.classList.toggle("fa-times-circle");
+}
 
-// Page Tabs //
-// const mobile = document.getElementById('mobile');
-
-// var burgerBtn = document.getElementById('burgerBtn');
-// var mobile = document.getElementById('mobile');
-// var demo1 = document.getElementById('demo1');
-// var demo2 = document.getElementById('demo2');
-// var demo3 = document.getElementById('demo3');
-
-// burgerBtn.addEventListener('click', function() {
-//   mobile.classList.toggle('navigation');
-// }, false);
-
-// mobile.classList.toggle('navigation');
-// Project List //
+function toggleMobileNav() {
+  const burgerBtn = document.getElementById("burgerBtn");
+  const navLinks = document.getElementById("navLinks");
+  if (
+    navLinks.style.visibility === "hidden" ||
+    navLinks.style.visibility === ""
+  ) {
+    navLinks.style.visibility = "visible";
+    toggleNavMenuIcon();
+  } else {
+    navLinks.style.visibility = "hidden";
+    toggleNavMenuIcon();
+  }
+  //   burgerBtn.classList.toggle("navMenuActive");
+}
