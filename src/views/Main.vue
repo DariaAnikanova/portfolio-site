cec082fe380bed038c53d6919afb6352d148cd4c
DariaAnikanova <daria.i.anikanova@gmail.com>
2022-03-30 15:51:20 -0400

    Added background

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,24 +1,29 @@
 <template>
-  <v-container>
-    <div class="d-flex">
-      <v-avatar size="200"
-        ><v-img
-          src="https://placekitten.com/200/300"
-          alt="Photo of Daria Anikanova"
-        ></v-img
-      ></v-avatar>
-      <div>
-        <h1 class="accent--text text-uppercase text-h4">Daria Anikanova</h1>
-        <h2 class="accent--text">Front End Developer</h2>
+  <v-container fluid fill-height class="main-bg px-2 pt-2 pb-0">
+    <v-container>
+      <div class="d-flex">
+        <v-avatar size="200"
+          ><v-img
+            src="https://placekitten.com/200/300"
+            alt="Photo of Daria Anikanova"
+          ></v-img
+        ></v-avatar>
+        <div>
+          <h1 class="accent--text text-uppercase text-h4">Daria Anikanova</h1>
+          <h2 class="accent--text">Front End Developer</h2>
+        </div>
       </div>
-    </div>
 
-    <div class="grid-container pa-0">
-      <project-card v-for="n in 3" :key="n" />
-    </div>
+      <div class="grid-container pa-0">
+        <project-card v-for="n in 3" :key="n" />
+      </div>
+    </v-container>
   </v-container>
 </template>
 <style lang="scss" scoped>
+.main-bg {
+  background: url("../assets/images/darianova.svg") round content-box;
+}
 .grid-container {
   display: grid;
   gap: 1rem;
